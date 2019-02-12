153ab4ee3dacbbe2b99c3439074bb6227a98fb85
Szymon Motyka <smotyka@pgs-soft.com>
2019-02-12 13:55:01 +0100

    backend moved to devops

--- providers.tf
+++ providers.tf
@@ -8,12 +8,12 @@ provider "azurerm" {
 #   subscription_id = "${var.azure_subscription_id}"
 #   tenant_id       = "${var.azure_tenant_id}"
 # }
-#Set the terraform backend
-terraform {
-  backend "azurerm" {
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    resource_group_name  = "backupresource"
-    storage_account_name = "smotykaterraform"
-  }
-}
+# #Set the terraform backend
+# terraform {
+#   backend "azurerm" {
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#     resource_group_name  = "backupresource"
+#     storage_account_name = "smotykaterraform"
+#   }
+# }
