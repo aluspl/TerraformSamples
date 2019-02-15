39b242c65b8ffce64f9559e79fbd5a8ecc2df544
Szymon Motyka <smotyka@pgs-soft.com>
2019-02-15 14:54:24 +0100

    cleaning in project

--- DscMetaConfigs/configuration.ps1
+++ DscMetaConfigs/configuration.ps1
@@ -72,8 +72,8 @@ Configuration DscMetaConfigs
 
              ResourceRepositoryWeb AzureAutomationStateConfiguration
              {
-             ServerUrl       = $RegistrationUrl
-             RegistrationKey = $RegistrationKey
+                 ServerUrl       = $RegistrationUrl
+                 RegistrationKey = $RegistrationKey
              }
          }
 
@@ -89,10 +89,10 @@ Configuration DscMetaConfigs
  # NOTE: DSC Node Configuration names are case sensitive in the portal.
  # TODO: edit the below as needed for your use case
 $Params = @{
-     RegistrationUrl = 'https://we-agentservice-prod-1.azure-automation.net/accounts/ad6dc2f3-12ce-4021-b315-e63e495f5c58';
-     RegistrationKey = 'sVT8TaPyHRgkWbgoVUONCq8tYki2U1e7OcxcZXGPH31QOx5lc42PnbD4TMmw85Mt3X1uI61Jz3qk5aZWUfcmYg==';
+     RegistrationUrl = 'x';
+     RegistrationKey = 'x';
      ComputerName = @('vm');
-     NodeConfigurationName = 'SimpleConfig.webserver';
+     NodeConfigurationName = 'InstallIIS.localhost';
      RefreshFrequencyMins = 30;
      ConfigurationModeFrequencyMins = 15;
      RebootNodeIfNeeded = $False;
